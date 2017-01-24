f991389151ceac693cf15e3d7dbd9031537c0f57
baayl <baayl@192.168.0.10>
2017-01-24 01:23:20 -0500

    Forgetting to Commit
    It cross my mind that I have to forgot that I have to commit
    
    
    Things that were done previously, 
    
    Drawing things
    
    reminder* for me
    g.fillOval instead of g.fillRect
    Instead of rectangles for the objects I made it as a "oval" -  circle
    Getting my red paddle as an oval  
    Getting my blue paddle as an oval 
    Getting my ball or hockey puck as an oval 
    
    
    Getting a background for the air hockey table
    
    I'm still figuring out how to import images right now it's now working.
    
    
    Still haven't reached the game physics stage will get to it.

---
+++ AirHockey/src/longword/Airhockey.java
@@ -0,0 +1,159 @@
+package longword;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author baayl
+ */
+public class Airhockey extends JComponent implements KeyListener {
+
+    // Height and Width of our game
+    static final int WIDTH = 800;
+    static final int HEIGHT = 600;
+    // sets the framerate and delay for our game
+    // you just need to select an approproate framerate
+    long desiredFPS = 60;
+    long desiredTime = (1000) / desiredFPS;
+    // making the hockey puck
+    Rectangle hockeyPuck = new Rectangle(WIDTH / 2, HEIGHT / 2, 30, 30);
+    int moveX = 10;
+    int moveY = 10;
+    int speed = 50;
+    // making the players "paddle"
+    Rectangle redPaddle = new Rectangle(300, HEIGHT / 2 - 25, 25, 25);
+    Rectangle bluePaddle = new Rectangle(500, HEIGHT / 2 - 25, 25, 25);
+    Rectangle blueGoal = new Rectangle(27, 255, 12, 100);
+    Rectangle redGoal = new Rectangle(768, 255, 12, 100);
+
+    BufferedImage background = loadImage("airhockeybackground.png");
+    int score1 = 0;
+    int score2 = 0;
+    
+        public BufferedImage loadImage(String filename) {
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(new File(filename));
+        } catch (Exception e) {
+            System.out.println("Error loading " + filename);
+        }
+        return img;
+    }
+    
+    // drawing of the game happens in here
+    // we use the Graphics object, g, to perform the drawing
+    // NOTE: This is already double buffered!(helps with framerate/speed)
+    @Override
+    public void paintComponent(Graphics g) {
+        // always clear the screen first!
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+
+        // GAME DRAWING GOES HERE 
+        g.drawImage(background, moveX, moveY, WIDTH, HEIGHT, null);
+        g.setColor(Color.BLACK);
+        g.fillRect(redGoal.x, redGoal.y, redGoal.width, redGoal.height);
+        g.fillRect(blueGoal.x, blueGoal.y, blueGoal.width, blueGoal.height);
+        g.fillOval(hockeyPuck.x, hockeyPuck.y, hockeyPuck.width, hockeyPuck.height);
+         g.setColor(Color.red);
+        g.fillOval(redPaddle.x, redPaddle.y, redPaddle.width, redPaddle.height);
+        g.setColor(Color.blue);
+        g.fillOval(bluePaddle.x, bluePaddle.y, bluePaddle.width, bluePaddle.height);
+               
+        
+
+        // GAME DRAWING ENDS HERE
+    }
+
+    // The main game loop
+    // In here is where all the logic for my game will go
+    public void run() {
+        // Used to keep track of time used to draw and update the game
+        // This is used to limit the framerate later on
+        long startTime;
+        long deltaTime;
+
+        // the main game loop section
+        // game will end if you set done = false;
+        boolean done = false;
+        while (!done) {
+            // determines when we started so we can keep a framerate
+            startTime = System.currentTimeMillis();
+
+            // all your game rules and move is done in here
+            // GAME LOGIC STARTS HERE 
+
+
+
+            // GAME LOGIC ENDS HERE 
+
+            // update the drawing (calls paintComponent)
+            repaint();
+
+
+
+            // SLOWS DOWN THE GAME BASED ON THE FRAMERATE ABOVE
+            // USING SOME SIMPLE MATH
+            deltaTime = System.currentTimeMillis() - startTime;
+            try {
+                if (deltaTime > desiredTime) {
+                    //took too much time, don't wait
+                    Thread.sleep(1);
+                } else {
+                    // sleep to make up the extra time
+                    Thread.sleep(desiredTime - deltaTime);
+                }
+            } catch (Exception e) {
+            };
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // creates a windows to show my game
+        JFrame frame = new JFrame("My Game");
+
+        // creates an instance of my game
+        Airhockey game = new Airhockey();
+        // sets the size of my game
+        game.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        // adds the game to the window
+        frame.add(game);
+
+        // sets some options and size of the window automatically
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        // shows the window to the user
+        frame.setVisible(true);
+
+        // starts my game loop
+        game.run();
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
